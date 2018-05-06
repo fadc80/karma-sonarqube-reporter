ec5653beff59c372107da271bc1c2b788b54a68c
Fernando Costa <fadc80@gmail.com>
2018-05-06 11:59:38 -0300

    Fixes #3

--- spec/lib/path-finder.spec.js
+++ spec/lib/path-finder.spec.js
@@ -1,9 +1,17 @@
 var mock = require('mock-require');
 
 var testData = {
-  'path/t1.spec.js':'describe("s1", function() { it("d1", function() {}); });',
-  'path/t2.spec.js':'describe("s2", function() { it("d2", function() {}); });',
-  'path/t3.spec.js':'describe("s3", function() { it("d3", function() {}); });' 	
+  'path/t1.spec.js':'describe(\'suite1\', function() { it(\'description1\', function() {}); });',
+  'path/t2.spec.js':'describe(\'suite2\', function() { it(\'description2\', function() {}); });',
+  'path/t3.spec.js':'describe(\'suite3\', function() { it(\'description3\', function() {}); });',	
+  'path/t4.spec.js':'describe(\'\\\'suite4\\\'\', function() { it(\'\\\'description4\\\'\', function() {}); });',
+  'path/t5.spec.js':'describe(\'\\\"suite5\\\"\', function() { it(\'\\\"description5\\\"\', function() {}); });',
+  'path/t6.spec.js':
+    'describe(\'suite6.1\', function() { it(\'description6.1\', function() {}); });' +
+    'describe(\'suite6.2\', function() { it(\'description6.2\', function() {}); });' + 
+    'describe(\'suite6.3\', function() { it(\'description6.3\', function() {}); });' +
+    'describe(\'suite6.4\', function() { describe(\'suite6.4.1\', function() { ' + 
+      'it(\'description6.4.1\', function() {}); }); });'   	
 }
 
 describe('Path finder tests', function() {
@@ -30,23 +38,67 @@ describe('Path finder tests', function() {
     mock.stop('fs');
   });
 
-  it('1st test file match suite s1 and description d1', function() {
-    expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
-      { suite: ['s1'], description: 'd1'})).toBe('path/t1.spec.js');
+  describe('Test files with single test cases', function() {
+
+    it('1st test file match suite 1 and description 1', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite1'], description: 'description1'})).toBe('path/t1.spec.js');
+    });
+
+    it('2sd test file match suite 2 and description 2', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite2'], description: 'description2'})).toBe('path/t2.spec.js');
+    });
+
+    it('3rd test file match suite 3 and description 3', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite3'], description: 'description3'})).toBe('path/t3.spec.js');
+    });
+
+    describe('Test cases with quoted text', function() {
+      
+      it('4rd test file match suite and description with single quotes', function() {
+        expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+          { suite: ['\'suite4\''], description: '\'description4\''})).toBe('path/t4.spec.js');
+      });
+
+       it('5th test file match suite and description with double quotes', function() {
+        expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+          { suite: ['\"suite5\"'], description: '\"description5\"'})).toBe('path/t5.spec.js');
+      });
+
+    });
+
   });
 
-  it('2sd test file match suite s2 and description d2', function() {
-    expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
-      { suite: ['s2'], description: 'd2'})).toBe('path/t2.spec.js');
-  });
+  describe('Test files with multiple test cases', function() {
+    
+    it('6th test file match suite 6.1 and description 6.1 (sibling)', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite6.1'], description: 'description6.2'})).toBe('path/t6.spec.js');
+    });
 
-  it('3rd test file match suite s3 and description d3', function() {
-    expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
-      { suite: ['s3'], description: 'd3'})).toBe('path/t3.spec.js');
+    it('6th test file match suite 6.2 and description 6.2 (sibling)', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite6.2'], description: 'description6.2'})).toBe('path/t6.spec.js');
+    });
+
+    it('6th test file match suite 6.3 and description 6.3 (sibling)', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite6.3'], description: 'description6.3'})).toBe('path/t6.spec.js');
+    });
+
+    it('6th test file match suite 6.4 and description 6.4 (nested)', function() {
+      expect(pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
+        { suite: ['suite6.4'], description: 'description6.4'})).toBe('path/t6.spec.js');
+    });
+
   });
 
   it('Test file not found', function() {
     expect(function() { pathFinder.testFilePath('**/*.spec.ts', 'utf-8', 
-      { suite: ['s4'], description: 'd4'})}).toThrow(new Error('Test file not found!'));
+      { suite: ['suiteNotFound'], description: 'descriptionNotFound'})})
+        .toThrow(new Error('Test file not found!'));
   });
+
 });
