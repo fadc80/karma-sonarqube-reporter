c95aac71884e900212bfa77ab8d77a4ac8e41872
Fabio <fabio.heer@bluewin.ch>
2020-06-17 03:50:31 +0200

    add type declaration file (#85)

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+import 'karma';
+
+export interface SonarqubeReporterConfiguration {
+    /** test files folder */
+    basePath: string;
+    /** test files glob pattern */
+    filePattern: string;
+    /** test files encoding */
+    encoding: string;
+    /** report destination */
+    outputFolder: string;
+    /** report for Sonarqube < 6.2 (disabled) */
+    legacyMode?: boolean;
+    /**
+     * report name callback, accepts also a string (reportName: 'report.xml') to generate a single file
+     *
+     * Report metadata array:
+     * - metadata[0] = browser name
+     * - metadata[1] = browser version
+     * - metadata[2] = plataform name
+     * - metadata[3] = plataform version
+     */
+    reportName: ((metadata: string[]) => string) | string;
+}
+
+declare module 'karma' {
+    interface ConfigOptions {
+        sonarqubeReporter?: SonarqubeReporterConfiguration;
+    }
+}
